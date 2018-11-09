487a7e483663ad832aa3c8f89867e4e9518ae83d
ttzztztz <935319340@qq.com>
2018-11-09 16:54:39 +0800

    eat

--- js/touch.js
+++ js/touch.js
@@ -18,14 +18,14 @@ function decidePicture(_touch_transform){
             else if (_touch_transform <= -60) hit =parseInt(hit) + 2;
             else if (_touch_transform <= -5) hit = parseInt(hit) + 1;
         }
+        if(hit>=touch_inside_translate.length) hit = hit - touch_inside_translate.length;
+        if(hit< 0) hit = parseInt(hit) + parseInt(touch_inside_translate.length);
         if(touch_now_hit != hit){
             let old_element = document.querySelector("li[data-opt='inside'][data-rank='"+touch_now_hit+"']");
             let new_element = document.querySelector("li[data-opt='inside'][data-rank='"+hit+"']");
             old_element.children[0].classList.remove("inside_item_active");
             new_element.children[0].classList.add("inside_item_active");
         }
-        if(hit>=touch_inside_translate.length) hit = hit - touch_inside_translate.length;
-        if(hit< 0) hit = parseInt(hit) + parseInt(touch_inside_translate.length);
         touch_now_hit = hit;
     }
     touch_transform = touch_inside_translate[hit];
@@ -42,10 +42,32 @@ function touch_start(event){
     touch_endX = touch.pageX;
     jtouch_inside.classList.remove("inside_transition");
 }
-function touch_move(event){
+function touch_move(event) {
     let touch = event.touches[0];
     touch_endX = touch.pageX;
-    jtouch_inside.style.transform = "translateX("+ (parseInt(touch_transform) + touch_px_to_vw( parseInt((touch_startX - touch_endX) * -1.0)))+"vw)";
+    let _pos = parseInt(touch_transform) + touch_px_to_vw(parseInt((touch_startX - touch_endX) * -1.0));
+
+    if (_pos >= 75) {
+        _pos = _pos - 150;
+        touch_transform = touch_transform - 100;
+    } else if (_pos <= -125) {
+        _pos = _pos + 150;
+        touch_transform = parseInt(touch_transform) + 100;
+    }
+
+    if(_pos>=25){
+        jtouch_inside_first.style.left = "0vw";
+        jtouch_inside_last.style.left = "-50vw";
+    } else if(_pos <= -75){
+        jtouch_inside_first.style.left = "200vw";
+        jtouch_inside_last.style.left = "150vw";
+    } else {
+        jtouch_inside_first.style.left = "0vw";
+        jtouch_inside_last.style.left = "150vw";
+    }
+
+    jtouch_inside.style.transform = "translateX(" + _pos + "vw)";
+
 }
 function touch_end(event){
     touch_moveX = touch_startX - touch_endX;
