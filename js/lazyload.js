4abf1ecf5846a3dc1f8596867706a5c2e066a43e
ttzztztz <935319340@qq.com>
2018-11-10 20:50:11 +0800

    lazyload

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,106 +1,136 @@
-(function () {
-    'use strict';
-    function throttle(fn, delay, mustRunDelay) {
-        var timer = null,
-            last;
-        return function () {
-            var context = this,
-                args = arguments,
-                now = +new Date();
-            if (last && (now - last) < mustRunDelay) {
-                clearTimeout(timer);
-                timer = setTimeout(function () {
-                    fn.apply(context, args);
-                    last = now;
-                }, delay);
-            } else {
-                last = now;
-                fn.apply(context, args);
-            }
+(function() {
+    var root = (typeof self == 'object' && self.self == self && self) ||
+        (typeof global == 'object' && global.global == global && global) ||
+        this || {};
+    Function.prototype.bind = Function.prototype.bind || function (context) {
+        if (typeof this !== "function") {
+            throw new Error("Function.prototype.bind - what is trying to be bound is not callable");
         }
-    }
-    function getViewPortHeight() {
-        return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || 0;
-    }
-    function getScrollTop() {
-        return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    }
-    function offsetTop(el) {
-        if (el) {
-            var t = el.offsetTop,
-                p = el.offsetParent;
-            while (p) {
-                if (window.navigator.userAgent.indexOf('MSIE 8.0') === -1) {
-                    t += p.clientTop;
+        var self = this;
+        var args = Array.prototype.slice.call(arguments, 1);
+        var fNOP = function () {};
+        var fBound = function () {
+            var bindArgs = Array.prototype.slice.call(arguments);
+            self.apply(this instanceof fNOP ? this : context, args.concat(bindArgs));
+        };
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+        return fBound;
+    };
+    var util = {
+        extend: function(target) {
+            for (var i = 1, len = arguments.length; i < len; i++) {
+                for (var prop in arguments[i]) {
+                    if (arguments[i].hasOwnProperty(prop)) {
+                        target[prop] = arguments[i][prop]
+                    }
                 }
-                t += p.offsetTop;
-                p = p.offsetParent;
             }
-            return t;
-        }
 
-    }
-    function isElementInViewport(el, offset) {
-        var h = offset || 20;
-        var box = el.getBoundingClientRect();
-        var top = (box.top >= 0),
-            left = (box.left >= 0),
-            bottom = (box.bottom <= (window.innerHeight || document.documentElement.clientHeight) + h),
-            right = (box.right <= (window.innerWidth || document.documentElement.clientWidth) + h);
-        return (top && left && bottom && right);
-    }
-    var count = 0,
-        lazyImages = [],
-        selector,
-        lazyClass,
-        offset;
-    function LazyLoad(options) {
-        options = options || {};
-        selector = options.selector || 'img';
-        lazyClass = options.lazyClass || 'data-src';
-        offset = options.offset || 0;
-        this._init();
-    }
-    LazyLoad.prototype = {
-        Constructor: LazyLoad,
-        _init: function () {
-            this._getLazyImages();
-            this._lazyLoad();
-            this._throttleLoad();
+            return target
         },
-        _getLazyImages: function () {
-            var imgEles = document.querySelectorAll(selector);
-            for (var j = 0; j < imgEles.length; j++) {
-                if (typeof (imgEles[j].getAttribute(lazyClass)) && imgEles[j].src==="img/loading.png") {
-                    lazyImages.push(imgEles[j]);
-                    count++;
-                }
+        addEvent: function(elem, type, fn) {
+            if (document.addEventListener) {
+                elem.addEventListener(type, fn, false);
+                return fn;
+            } else if (document.attachEvent) {
+                var bound = function() {
+                    return fn.apply(elem, arguments)
+                };
+                elem.attachEvent('on' + type, bound);
+                return bound;
             }
         },
-        _lazyLoad: function () {
-            if (!count) return;
-            for (var i = 0; i < lazyImages.length; i++) {
-                var lazyImg = lazyImages[i];
-                if (isElementInViewport(lazyImg)) {
-                    lazyImg.src = lazyImg.getAttribute(lazyClass);
-                    lazyImg.removeAttribute(lazyClass);
-                    lazyImages.splice(i, 1);
-                    count--;
-                }
+        removeEvent: function(elem, type, fn) {
+            if (document.removeEventListener) {
+                elem.removeEventListener(type, fn, false)
             }
-        },
-        _throttleLoad: function () {
-            window.onscroll = window.onresize = throttle(this._lazyLoad, 100, 200);
+            else {
+                elem.detachEvent("on" + type, fn)
+            }
         }
     };
-
-    window.LazyLoad = LazyLoad;
-})();
-
-window.onload = function () {
-    new LazyLoad({
-        selector: 'img',
-        lazyClass: 'data-src',
-        offset: 300
-    });
-};
+    function Lazy(opts) {
+        this.opts = util.extend({}, this.constructor.defaultOpts, opts);
+        this.init();
+    }
+    Lazy.defaultOpts = {
+        delay: 250,
+        useDebounce: false
+    };
+    var proto = Lazy.prototype;
+    proto.init = function() {
+        this.calulateView();
+        this.bindScrollEvent();
+    };
+    proto.calulateView = function() {
+        this.view = {
+            top: 0 - (parseInt(this.opts.top, 10) || 0),
+            bottom: (root.innerHeight || document.documentElement.clientHeight) + (parseInt(this.opts.bottom, 10) || 0),
+            left: 0 - (parseInt(this.opts.left, 10) || 0),
+            right: (root.innerWidth || document.documentElement.clientWidth) + (parseInt(this.opts.right, 10) || 0)
+        }
+    };
+    proto.bindScrollEvent = function() {
+        var scrollEvent = util.addEvent(root, 'scroll', this.handleLazyLoad.bind(this));
+        var loadEvent = util.addEvent(root, 'load', this.handleLazyLoad.bind(this));
+        this.event = {
+            scrollEvent: scrollEvent,
+            loadEvent: loadEvent
+        }
+    };
+    var timer = null;
+    proto.handleLazyLoad = function() {
+        var self = this;
+        if (!this.opts.useDebounce && !!timer) {
+            return;
+        }
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            timer = null;
+            self.render()
+        }, this.opts.delay);
+    };
+    proto.isHidden = function(element) {
+        return (element.offsetParent === null);
+    };
+    proto.checkInView = function(element) {
+        if (this.isHidden(element)) {
+            return false;
+        }
+        var rect = element.getBoundingClientRect();
+        return (rect.right >= this.view.left && rect.bottom >= this.view.top && rect.left <= this.view.right && rect.top <= this.view.bottom);
+    };
+    proto.render = function() {
+        var nodes = document.querySelectorAll('[data-src]');
+        var length = nodes.length;
+        for (var i = 0; i < length; i++) {
+            elem = nodes[i];
+            if (this.checkInView(elem)) {
+                if (elem.src !== (src = elem.getAttribute('data-src'))) {
+                    elem.src = src;
+                }
+                elem.removeAttribute('data-src');
+                if (this.opts.onload && typeof this.opts.onload === 'function') {
+                    this.opts.onload(elem);
+                }
+            }
+        }
+        if (!length) {
+            this.unbindScrollEvent();
+        }
+    };
+    proto.unbindScrollEvent = function(){
+        util.removeEvent(root, 'scroll', this.event.scrollEvent)
+        util.removeEvent(root, 'load', this.event.loadEvent)
+    };
+    if (typeof exports != 'undefined' && !exports.nodeType) {
+        if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+            exports = module.exports = Lazy;
+        }
+        exports.Lazy = Lazy;
+    } else {
+        root.Lazy = Lazy;
+    }
+}());
+new Lazy();
